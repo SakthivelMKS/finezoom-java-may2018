c4e6bce094be88fda76350a7211e589405635938
SakthivelMKS <37956479+SakthivelMKS@users.noreply.github.com>
2018-06-05 20:39:29 +0530

    code commit;

--- hello-java/src/com/finezoom/com/AverageArrayProgram.java
+++ hello-java/src/com/finezoom/com/AverageArrayProgram.java
@@ -3,7 +3,7 @@ package com.finezoom.com;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArraySum {
+public class AverageArrayProgram {
 	// Array sum
 	public static void main(String[] args) {
 		//variable and array declaration
--- hello-java/src/com/finezoom/com/PrimeNumber.java
+++ hello-java/src/com/finezoom/com/PrimeNumber.java
@@ -3,15 +3,15 @@ package com.finezoom.com;
 import java.util.Scanner;
 
 public class PrimeNumber {
-	//main method
+	// main method
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Number");
-		//variable are initialization
+		// variable are initialization
 		boolean isPrime = true;
 		int number, temp;
 		number = sc.nextInt();
-		//Logic apply for checking prime number
+		// Logic apply for checking prime number
 		for (int i = 2; i <= number / 2; i++) {
 			temp = number % i;
 			if (temp == 0) {
