b773b0acb9324178506c2e00ab37cdef5a57f0a9
SakthivelMKS <37956479+SakthivelMKS@users.noreply.github.com>
2018-06-15 02:25:41 +0530

    Reading File

--- hello-java/src/com/finezoom/oops/TeacherMain.java
+++ hello-java/src/com/finezoom/oops/TeacherMain.java
@@ -1,5 +1,9 @@
 package com.finezoom.oops;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -9,11 +13,37 @@ public class TeacherMain extends TeacherImplent {
 	public static void main(String[] args) {
 		Teacher tech = null;
 
-		
 		TeacherMain techMain = new TeacherMain();
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter The Array Size");
+
 		ArrayList<Teacher> arrayList = new ArrayList<Teacher>();
+		FileReader fr;
+		Teacher techs = null;
+
+		// stream = new BufferedReader(new FileReader(csvFileName));
+		try {
+			fr = new FileReader("D:/Java-Trainning/trunk/teacher.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				String[] data = line.split(",");
+				techs = new Teacher();
+				// String id=;
+				techs.setId(Integer.parseInt(data[0]));
+				techs.setName(data[1]);
+				techs.setAge(data[2]);
+				techs.setAddress(data[3]);
+
+				arrayList.add(techs);
+			}
+
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("Enter The Array Size");
 		int arraySize = scan.nextInt();
 		for (int i = 0; i < arraySize; i++) {
 			tech = new Teacher();
@@ -29,9 +59,9 @@ public class TeacherMain extends TeacherImplent {
 			arrayList.add(tech);
 		}
 
-	/*	ArrayList<Teacher> teach = techMain.add(arrayList);
+		ArrayList<Teacher> teachAddedList = techMain.add(arrayList);
 
-		for (Teacher tt : teach) {
+		for (Teacher tt : teachAddedList) {
 
 			System.out.println(tt.getId());
 			System.out.println(tt.getName());
@@ -41,17 +71,17 @@ public class TeacherMain extends TeacherImplent {
 		}
 		System.out.println("Enter the teacher Id");
 		int id = scan.nextInt();
-		ArrayList<Teacher> techDetails = techMain.find(arrayList, id);
-		for (Teacher object : techDetails) {
+		ArrayList<Teacher> techFindDetail = techMain.find(arrayList, id);
+		for (Teacher object : techFindDetail) {
 			System.out.println(object.getId());
 			System.out.println(object.getName());
 			System.out.println(object.getAge());
 			System.out.println(object.getAddress());
-		}*/
-		
+		}
+
 		System.out.println("Enter the teacher Id");
 		int updateId = scan.nextInt();
-		ArrayList<Teacher> updateList =techMain.update(updateId, arrayList);
+		ArrayList<Teacher> updateList = techMain.update(updateId, arrayList);
 		for (Teacher updateData : updateList) {
 
 			System.out.println(updateData.getId());
@@ -60,18 +90,17 @@ public class TeacherMain extends TeacherImplent {
 			System.out.println(updateData.getAddress());
 
 		}
-		//System.out.println("Enter the teacher Id");
-		//int removeid = scan.nextInt();
-		/*ArrayList<Teacher> remove = techMain.remove(removeid,arrayList);
-		for (Teacher tt : remove) {
+		System.out.println("Enter the teacher Id");
+		int removeid = scan.nextInt();
+		ArrayList<Teacher> removedList = techMain.remove(removeid, arrayList);
+		for (Teacher tt : removedList) {
 
 			System.out.println(tt.getId());
 			System.out.println(tt.getName());
 			System.out.println(tt.getAge());
 			System.out.println(tt.getAddress());
 
-		}*/
+		}
 
 	}
-
 }
