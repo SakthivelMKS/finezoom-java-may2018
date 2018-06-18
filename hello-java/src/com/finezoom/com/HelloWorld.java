fb848feb5f551696fd3fcab564810af8450e884d
SakthivelMKS <37956479+SakthivelMKS@users.noreply.github.com>
2018-06-18 23:04:46 +0530

    remove

--- hello-java/src/com/finezoom/com/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.finezoom.com;
-
-class HelloWorld {
-	//Main method
-	public static void main(String argd[]) {
-		//Displaying Comment
-		System.out.println("HelloWorld");
-	}
-}
--- hello-java/src/com/finezoom/exception/ExceptionUtil.java
+++
@@ -1,9 +0,0 @@
-package com.finezoom.exception;
-
-public class  ExceptionUtil {
-	
-	public static void divison(int a)throws Exception {
-		int c=a/0;
-	}
-
-}
